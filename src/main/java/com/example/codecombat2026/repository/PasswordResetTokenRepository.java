ac8f6814f421f05c52def8f288246cc519633a93
Pritam Mandal <2305554@kiit.ac.in>
2026-01-02 02:12:37 +0530

    fix: Added @Transactional annotation to password reset token deletion methods

--- src/main/java/com/example/codecombat2026/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/example/codecombat2026/repository/PasswordResetTokenRepository.java
@@ -4,6 +4,7 @@ import com.example.codecombat2026.entity.PasswordResetToken;
 import com.example.codecombat2026.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -13,7 +14,9 @@ public interface PasswordResetTokenRepository extends JpaRepository<PasswordRese
 
     Optional<PasswordResetToken> findByToken(String token);
 
+    @Transactional
     void deleteByExpiryDateBefore(LocalDateTime date);
 
+    @Transactional
     void deleteByUser(User user);
 }
